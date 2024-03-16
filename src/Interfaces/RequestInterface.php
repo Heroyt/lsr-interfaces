8474879f8df3388f7567a5b033c5e5318deeab47
Tomáš Vojík <vojik@wboy.cz>
2024-03-16 18:33:28 +0100

    :sparkles: Updated the RequestInterface to help transition to PSR-7 requests
    Signed-off-by: Tomáš Vojík <vojik@wboy.cz>

--- src/Interfaces/RequestInterface.php
+++ src/Interfaces/RequestInterface.php
@@ -21,18 +21,23 @@ interface RequestInterface extends JsonSerializable
 	 * @return void
 	 * @throws RouteNotFoundException
 	 */
-	public function handle() : void;
+	public function handle(): void;
 
-	public function getRoute() : ?RouteInterface;
+	public function getRoute(): ?RouteInterface;
 
 	/**
 	 * @return string[]
 	 */
-	public function getPath() : array;
+	public function getPath(): array;
 
 	public function getType(): RequestMethod;
 
-	public function getMethod(): string;
+	/**
+	 * @param array<string,string|numeric|null> $params
+	 *
+	 * @return $this
+	 */
+	public function setParams(array $params): static;
 
 	/**
 	 * @param string     $name
@@ -42,34 +47,34 @@ interface RequestInterface extends JsonSerializable
 	 */
 	public function getParam(string $name, mixed $default = null): string|int|float|null;
 
-	public function setPreviousRequest(RequestInterface $request) : static;
+	public function setPreviousRequest(RequestInterface $request): static;
 
-	public function addError(string $error) : static;
+	public function addError(string $error): static;
 
-	public function addPassError(string $error) : static;
-
-	/**
-	 * @return string[]
-	 */
-	public function getErrors() : array;
+	public function addPassError(string $error): static;
 
 	/**
 	 * @return string[]
 	 */
-	public function getPassErrors() : array;
-
-	public function addNotice(string $notice) : static;
-
-	public function addPassNotice(string $notice) : static;
+	public function getErrors(): array;
 
 	/**
 	 * @return string[]
 	 */
-	public function getNotices() : array;
+	public function getPassErrors(): array;
+
+	public function addNotice(string $notice): static;
+
+	public function addPassNotice(string $notice): static;
 
 	/**
 	 * @return string[]
 	 */
-	public function getPassNotices() : array;
+	public function getNotices(): array;
+
+	/**
+	 * @return string[]
+	 */
+	public function getPassNotices(): array;
 
 }
