ea25854cb3dce564df958f3ae0e11d943cad4f47
Tomáš Vojík <vojik@wboy.cz>
2025-01-10 15:39:44 +0100

    :sparkles: New RedirectException - added previous request parameter

--- src/Exceptions/RedirectException.php
+++ src/Exceptions/RedirectException.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace Lsr\Exceptions;
 
+use Lsr\Core\Requests\Request;
+
 /**
  * @phpstan-type RedirectCode int<300,308>
  *
@@ -14,10 +16,12 @@ class RedirectException extends \RuntimeException
 	/**
 	 * @param non-empty-string|array<int|string, string> $url
 	 * @param RedirectCode                               $code
+	 * @param Request|null $request
 	 */
 	public function __construct(
-		public readonly string|array $url,
-		int                          $code = 302
+		public readonly string|array $url = [],
+		int                          $code = 302,
+		public readonly ?Request     $request = null,
 	) {
 		parent::__construct('Redirect to: ' . json_encode($url), $code);
 	}
